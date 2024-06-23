5580fa44f3de90fcf440f9814665941c4bc828e3
Steph <145380074+steph-sketch850@users.noreply.github.com>
2024-06-23 13:17:16 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,36 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const getNumberOfLines = parseFloat(lines);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
